8ac063ffec37b7ff78cd567f8956135fd7079368
«Diana» <diana-mk@ya.ru>
2022-09-07 22:12:42 +0700

    домашняя работа 9

---
+++ task64/Program.cs
@@ -0,0 +1,6 @@
+﻿string Queue(int M, int N)
+{
+	if (M <= N) return $"{M} {Queue(M + 1, N)}";
+	else return string.Empty;
+}
+System.Console.WriteLine(Queue(2, 8));
---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30 */
+/* int SumNum(int M, int N)
+{
+	int sum = 0;
+	for (int i = M; i <= N; i++)
+	{
+		sum = sum + i;
+	}
+	return sum;
+}
+System.Console.WriteLine(SumNum(4, 8)); */
+
+int SumNum2(int M, int N)
+{
+	if (M <= N) return M + SumNum2(M + 1, N);
+	else return 0;
+}
+System.Console.WriteLine(SumNum2(4, 8));
